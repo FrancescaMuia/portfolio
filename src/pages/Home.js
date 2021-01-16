522ece211ee57fd704d66cc37febb4cb2d1da971
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2021-01-16 19:39:13 +0100

    feat(buttonandbox)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,22 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 import ImageGallery from "../components/ImageGallery"
 
 export function Home() {
     return (
         <main className="homeClass">
             <ImageGallery />
-            <section>
-                <hr className="accessory"></hr>
+            <NavLink to="/projects"
+                className="projectsButton">
+                Projects
+                    </NavLink>
+            <section className="paragraphSection">
+
 
                 <p className="homeParagraph"><p className="homeTitle">Hello!</p>
                 I'm Francesca, a freelance illustrator and graphic designer based in Italy.
                 </p>
-                <hr className="accessory"></hr>
+
             </section>
         </main>
     )
