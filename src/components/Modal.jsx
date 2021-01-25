9d7271a2a674e5066d34bcc6202cf44ddfe31295
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2021-01-25 20:43:53 +0100

    fix(modal)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,10 +8,10 @@ export default class Modal extends React.Component {
         }
         console.log(this.props);
         return (
-            <div className="modalClass flex" style={{ width: "40%", height: "100%", backgroundColor: "green", justifyContent: "center", alignItems: "center" }}>
-                <img src={this.props.name} style={{ width: "85%" }} onClick={
+            <div className="modalClass flex" style={{ width: "100%", height: "100%", backgroundColor: "#fffaf2", justifyContent: "center", alignItems: "center" }}>
+                <img src={this.props.name} style={{ width: "30%" }} onClick={
                     this.props.onClose
-                } />;
+                } />
             </div>
         )
     }
