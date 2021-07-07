81dd26205fb2e2fb26c4cc68f1bdbdcdef5ad9f5
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2021-07-07 20:22:30 +0200

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,14 @@
 import React from "react"
-
+import profilo from "../images/Img profilo.jpg"
 export function About() {
-    return <h1>About Page</h1>
+    return (
+        <main style={{ display: "flex", justifyContent: "space-between", margin: "5% 14%" }} className="homeClass">
+            <div style={{ width: "49%", borderStyle: "solid", borderWidth: "3px" }}>
+                <img style={{ width: "100%" }} src={profilo} />
+            </div>
+            <div style={{ width: "49%", borderStyle: "solid", borderWidth: "3px" }}>
+                ciao
+            </div>
+        </main>
+    )
 }
