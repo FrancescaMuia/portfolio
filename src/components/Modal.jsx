2e9b434ce72a195f7aa339fa22c238ca270c9944
FraEl11 <52604627+FraEl11@users.noreply.github.com>
2021-07-31 20:32:35 +0200

    gallery fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import avocado from "../images/avocado.jpg"
 import bowie from "../images/bowie.jpg"
-import capital_N from "../images/capital_N.jpg"
 import coffee from "../images/coffee.jpg"
 import Mille from "../images/Mille.JPG"
-import dtiys_alona from "../images/dtiys_alona.jpg"
-import GB from "../images/GB.jpg"
 import girl from "../images/girl.jpg"
 import loki from "../images/Loki.JPG"
 import rumore from "../images/Rumore.JPG"
+import do_it from "../images/Do_it.JPG"
+import dancingqueen from "../images/DancingQueen.JPG"
+import signora from "../images/Signora.JPG"
+import me_so_rotta from "../images/me_so_rotta.jpg"
+import treat_people_with_kindness from "../images/treat_people_with_kindness.jpg"
+
 import $ from 'jquery';
 
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default class Modal extends React.Component {
 
@@ -28,21 +33,27 @@ export default class Modal extends React.Component {
             imgs: [
                 loki,
                 rumore,
-                avocado,
-                bowie,
-                capital_N,
-                coffee,
+                do_it,
+                dancingqueen,
                 Mille,
-                dtiys_alona,
-                GB,
-                girl
+                signora,
+                me_so_rotta,
+                treat_people_with_kindness,
+                girl,
+                bowie,
+                avocado,
+                coffee
             ]
         };
 
         // Bind your class method in the constructor
         this.handleClick = this.handleClick.bind(this);
     }
-
+    componentDidMount() {
+        AOS.init({
+            duration: 500
+        })
+    }
     // Here we get the length of the imgs array, and the current
     // pointer position. If the pointer is at the end of the array
     // set it back to zero, otherwise increase it by one.
@@ -88,24 +99,36 @@ export default class Modal extends React.Component {
         }
 
         const { pointer, imgs } = this.state;
-
+        let isArrow = false;
         // this.setState({ pointer: this.props.start });
 
 
 
         return (
-            <div className="modalClass flex">
-                <p className="arrow" style={{ float: "left" }} onClick={() => {
-                    this.handleClick(this.props.start, false);
-                    this.clicked(true);
-                    this.restart();
-                }}>&#60;</p>
-                <img id="target" className="galleryImage" src={imgs[this.state.bool === false ? this.props.start : pointer]} style={{ width: "35%" }} onClick={() => {
+            <div className="modalClass flex" onClick={() => {
+                isArrow = false;
+                if (isArrow === false) {
                     this.clicked(false);
 
                     this.props.onClose();
                 }
-                } />
+            }
+            }>
+                <p className="arrow" style={{ float: "left" }} onClick={() => {
+                    isArrow = true;
+                    this.handleClick(this.props.start, false);
+                    this.clicked(true);
+                    this.restart();
+                }}>&#60;</p>
+                <img id="target" className="galleryImage" data-aos="fade-zoom-in"
+                    data-aos-easing="ease-in-back"
+                    data-aos-delay="300"
+                    data-aos-offset="0" src={imgs[this.state.bool === false ? this.props.start : pointer]} style={{ width: "35%" }} onClick={() => {
+                        this.clicked(false);
+
+                        this.props.onClose();
+                    }
+                    } />
                 <p className="arrow" onClick={() => {
                     this.handleClick(this.props.start, true);
                     this.clicked(true);
--- src/components/ProjectsImage.jsx
+++ src/components/ProjectsImage.jsx
@@ -1,25 +1,25 @@
 import { Component } from 'react';
 import Modal from "./Modal";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import sito_avocado from "../images/sito_avocado.jpg"
 import avocado from "../images/avocado.jpg"
 import bowie from "../images/bowie.jpg"
-import capital_N from "../images/capital_N.jpg"
 import coffee from "../images/coffee.jpg"
 import mille from "../images/Mille.JPG"
-import dtiys_alona from "../images/dtiys_alona.jpg"
-import gb from "../images/GB.jpg"
 import girl from "../images/girl.jpg"
-import lhascrittounafemmina from "../images/lhascrittounafemmina.jpg"
-import sfondo from "../images/sfondo.jpg"
 import treat_people_with_kindness from "../images/treat_people_with_kindness.jpg"
 import rumore from "../images/Rumore.JPG"
 import loki from "../images/Loki.JPG"
+import do_it from "../images/Do_it.JPG"
+import dancingqueen from "../images/DancingQueen.JPG"
+import signora from "../images/Signora.JPG"
+import me_so_rotta from "../images/me_so_rotta.jpg"
 
 
 export default class ProjectsImage extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             show: false,
             img: ""
@@ -28,6 +28,11 @@ export default class ProjectsImage extends Component {
 
     }
 
+    componentDidMount() {
+        AOS.init({
+            duration: 1000
+        })
+    }
     showModal = (img) => {
         console.log(img);
         this.setState(prev => ({
@@ -48,100 +53,164 @@ export default class ProjectsImage extends Component {
 
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${loki})` }} onClick={e => {
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${loki})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
                         this.showModal(0);
                     }}>
-
+                        <div class="text">Loki</div>
                     </div>
 
                 </div>
             </div>
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${rumore})` }} onClick={e => {
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${rumore})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
                         this.showModal(1);
                     }}>
-
+                        <div class="text">Rumore</div>
                     </div>
 
                 </div>
             </div>
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${avocado})` }} onClick={e => {
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${do_it})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
                         this.showModal(2);
                     }}>
-
+                        <div class="text">Do it</div>
                     </div>
 
                 </div>
             </div>
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${bowie})` }} onClick={e => {
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${dancingqueen})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
                         this.showModal(3);
                     }}>
-
+                        <div class="text">Dancing Queen</div>
                     </div>
 
                 </div>
             </div>
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${capital_N})` }} onClick={e => {
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${mille})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
                         this.showModal(4);
                     }}>
-
+                        <div class="text">Mille</div>
                     </div>
 
                 </div>
             </div>
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${coffee})` }} onClick={e => {
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${signora})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
                         this.showModal(5);
                     }}>
-
+                        <div class="text">Signoraaa!</div>
                     </div>
 
                 </div>
             </div>
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${mille})` }} onClick={e => {
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${me_so_rotta})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
                         this.showModal(6);
                     }}>
+                        <div class="text">Fuori dai denti<br />"Me so rotta er ca"</div>
                     </div>
 
                 </div>
             </div>
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${dtiys_alona})` }} onClick={e => {
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${treat_people_with_kindness})` }}>
+
+                    </div>
+                    <div class="overlay" onClick={e => {
                         this.showModal(7);
                     }}>
+                        <div class="text">Treat People with Kindness</div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="responsive">
+                <div className="gallery">
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${girl})` }} >
+                    </div>
+                    <div class="overlay" onClick={e => {
+                        this.showModal(8);
+                    }}>
+                        <div class="text">Tropical Summer</div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="responsive">
+                <div className="gallery">
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${bowie})` }}>
+
+                    </div>
+                    <div class="overlay" onClick={e => {
+                        this.showModal(9);
+                    }}>
+                        <div class="text">Bowie</div>
                     </div>
 
                 </div>
             </div>
             <div className="responsive">
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${gb})` }} onClick={e => {
-                        this.showModal(8);
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${avocado})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
+                        this.showModal(10);
                     }}>
+                        <div class="text">Avocado</div>
                     </div>
 
                 </div>
             </div>
+
             <div className="responsive" style={{ paddingBottom: "10%" }}>
                 <div className="gallery">
-                    <div className="image-style" style={{ backgroundImage: `url(${girl})` }} onClick={e => {
-                        this.showModal(9);
+                    <div className="image-style" data-aos="fade-up" style={{ backgroundImage: `url(${coffee})` }} >
+
+                    </div>
+                    <div class="overlay" onClick={e => {
+                        this.showModal(11);
                     }}>
+                        <div class="text">Coffee</div>
                     </div>
 
                 </div>
             </div>
+
+
         </div>)
     }
 }
